f96bd05711f87f33c99da523914f564d5666d041
NeilPandya <NeilPandya@users.noreply.github.com>
2022-03-20 01:57:28 -0500

    Added getDatasets.sh

--- getDatasets.sh
+++ getDatasets.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cd /darknet
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1xlzzjHlZViof6s8W_EHHLHZkiLN4rAST' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1nzbagwoCKMHCY_Jvcc2pfQVHnjFx66FY" -O datasets.zip && rm -rf /tmp/cookies.txt
-unzip -q datasets.zip
-rm /Mask_RCNN/datasets.zip
-chmod 777 -R /Mask_RCNN/datasets
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1xlzzjHlZViof6s8W_EHHLHZkiLN4rAST' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1xlzzjHlZViof6s8W_EHHLHZkiLN4rAST" -O datasets.zip && rm -rf /tmp/cookies.txt
+unzip -q -y datasets.zip
+rm datasets.zip
+chmod 777 -R datasets
